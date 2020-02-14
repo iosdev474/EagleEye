22c3e4a60595c2599f71fb80c991aa5f3e5f6e9b
iOSDev474 <iosdev474@icloud.com>
2020-02-15 01:32:01 +0530

    Add parsing of players
    Players data is extracted from demo file and is stored in a file

--- Parser/parser.js
+++ Parser/parser.js
@@ -3,14 +3,45 @@ demofile = require("demofile");
 
 fs.readFile("test.dem", (err, buffer) => {
   const demoFile = new demofile.DemoFile();
+  myID = -1;
+  console.log("Start");
+
+  demoFile.stringTables.on("update", e => {
+    if (e.table.name === "userinfo" && e.userData != null) {
+      if(e.userData.name == 'n00bie'){
+        myID = e.userData.userId;
+      }
+    }
+  });
+
+  demoFile.gameEvents.on("player_hurt", e => {
+    if(myID != -1 && e.attacker==myID){
+      const attacker = demoFile.entities.getByUserId(e.attacker);
+      const attackerName = attacker ? attacker.name : "unnamed";
+      const victim = demoFile.entities.getByUserId(e.userid);
+      const victimName = victim ? victim.name : "unnamed";
+      data = attacker.position.x + "," + attacker.position.y + "," + attacker.position.z + "," + attacker.eyeAngles.pitch + "," + attacker.eyeAngles.yaw + "," + (attacker.isDucked?"1":"0") + "," + victim.position.x + "," + victim.position.y + "," + victim.position.z + "," + victim.eyeAngles.pitch + "," + victim.eyeAngles.yaw + "," + (victim.isDucked?"1":"0") + "\n";
+
+      fs.appendFile("data_tagged.txt", data, (err) => {
+        if (err) console.log(err);
+        console.log(data+"Successfully Written to File.\n");
+      });
+    }
+  });
 
   demoFile.gameEvents.on("player_death", e => {
-    const victim = demoFile.entities.getByUserId(e.userid);
-    const victimName = victim ? victim.name : "unnamed";
-    const attacker = demoFile.entities.getByUserId(e.attacker);
-    const attackerName = attacker ? attacker.name : "unnamed";
-    const headshotText = e.headshot ? " HS" : "";
-    console.log(`${attackerName} [${e.weapon}${headshotText}] ${victimName}`);
+    if(myID != -1 && e.attacker==myID){
+      const attacker = demoFile.entities.getByUserId(e.attacker);
+      const attackerName = attacker ? attacker.name : "unnamed";
+      const victim = demoFile.entities.getByUserId(e.userid);
+      const victimName = victim ? victim.name : "unnamed";
+      data = attacker.position.x + "," + attacker.position.y + "," + attacker.position.z + "," + attacker.eyeAngles.pitch + "," + attacker.eyeAngles.yaw + "," + (attacker.isDucked?"1":"0") + "," + victim.position.x + "," + victim.position.y + "," + victim.position.z + "," + victim.eyeAngles.pitch + "," + victim.eyeAngles.yaw + "," + (victim.isDucked?"1":"0") + "," + e.penetrated + "," + (e.headshot?"1":"0") + "\n";
+
+      fs.appendFile("data_kill.txt", data, (err) => {
+        if (err) console.log(err);
+        console.log(data+"Successfully Written to File.\n");
+      });
+    }
   });
 
   demoFile.parse(buffer);
